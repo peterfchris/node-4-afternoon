7341a1de4315ec8be9195b5aec0a8faac62cc4ee
Peter Christensen <peterchristensen@Peters-MacBook-Pro.local>
2019-05-21 14:00:59 -0600

    steps 1 - 4

---
+++ server/index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+app.use(express.json())
+const {SERVER_PORT, SESSION_SECRET} = process.env
+const session = require('express-session')
+const checkForSession = require('./middlewares/checkForSession')
+
+app.use(session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60
+    }
+}))
+
+app.use(checkForSession)
+
+app.listen(SERVER_PORT, () => console.log(`Nothing is broken yet on ${SERVER_PORT}`))
