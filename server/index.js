4b27ed003fcce24e6b641700b644119c1a45c52c
Peter Christensen <peterchristensen@Peters-MacBook-Pro.local>
2019-05-21 14:30:26 -0600

    step 7

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const {SERVER_PORT, SESSION_SECRET} = process.env
 const session = require('express-session')
 const checkForSession = require('./middlewares/checkForSession')
 const swagController = require('./controllers/swagController')
+const authController = require('./controllers/authController')
 
 app.use(session({
     secret: SESSION_SECRET,
@@ -21,3 +22,7 @@ app.use(checkForSession)
 app.listen(SERVER_PORT, () => console.log(`Nothing is broken yet on ${SERVER_PORT}`))
 
 app.get('/api/swag', swagController.read)
+app.get('/api/user', authController.getUser)
+app.post('/api/login', authController.login)
+app.post('/api/register', authController.register)
+app.post('/api/signout', authController.signout)
